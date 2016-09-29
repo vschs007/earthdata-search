794a6f193be5c1a8c4554452fa1d8306f75ecb46
Vivek Kumar Singh <vschs007@gmail.com>
2016-09-29 07:38:12 -0700

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,6 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
       { pattern: './spec/setup.js', included: true},
